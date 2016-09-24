66ea8cf4d25916c18e9ab72454070eb94b03d210
Neil <754931220@qq.com>
2016-09-24 10:54:52 +0800

    学习

---
+++ WinSocket/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WinSocket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Request = new System.Windows.Forms.Button();
+            this.txt_Request = new System.Windows.Forms.TextBox();
+            this.txt_Response = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Request
+            // 
+            this.btn_Request.Location = new System.Drawing.Point(12, 12);
+            this.btn_Request.Name = "btn_Request";
+            this.btn_Request.Size = new System.Drawing.Size(75, 23);
+            this.btn_Request.TabIndex = 0;
+            this.btn_Request.Text = "请求";
+            this.btn_Request.UseVisualStyleBackColor = true;
+            this.btn_Request.Click += new System.EventHandler(this.btn_Request_Click);
+            // 
+            // txt_Request
+            // 
+            this.txt_Request.Location = new System.Drawing.Point(12, 41);
+            this.txt_Request.Multiline = true;
+            this.txt_Request.Name = "txt_Request";
+            this.txt_Request.Size = new System.Drawing.Size(216, 367);
+            this.txt_Request.TabIndex = 1;
+            // 
+            // txt_Response
+            // 
+            this.txt_Response.Location = new System.Drawing.Point(257, 41);
+            this.txt_Response.Multiline = true;
+            this.txt_Response.Name = "txt_Response";
+            this.txt_Response.Size = new System.Drawing.Size(216, 367);
+            this.txt_Response.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 420);
+            this.Controls.Add(this.txt_Response);
+            this.Controls.Add(this.txt_Request);
+            this.Controls.Add(this.btn_Request);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Request;
+        private System.Windows.Forms.TextBox txt_Request;
+        private System.Windows.Forms.TextBox txt_Response;
+    }
+}
+
---
+++ WinSocket/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace WinSocket
+{
+    public partial class Form1 : Form
+    {
+        public int threadSendToServerErrorCount = 0;
+        private static Thread threadSendToServer;
+        private int gloryh = 0;//程序与设备的会话句柄  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        byte[] GetConMessage(MemoryStream ms)
+        {
+            string content = Encoding.UTF8.GetString(ms.ToArray());
+            //string message = string.Format("[len={0}]{1}", content.Length, content);//添加消息结束标记  
+            return Encoding.UTF8.GetBytes(content);
+        }
+
+        public string GetIpAddress(string input)
+        {
+            return input.Split(':')[0];
+        }
+
+        public class ConnectionDataMessage
+        {
+            ///// <summary>  
+            ///// ip地址  
+            ///// </summary>  
+            //public string Address { get; set; }
+            ///// <summary>  
+            ///// 登录用户  
+            ///// </summary>  
+            //public string SignInName { get; set; }
+            ///// <summary>  
+            ///// 设备号  
+            ///// </summary>  
+            //public string DeviceCode { get; set; }
+            ///// <summary>  
+            ///// 网点  
+            ///// </summary>  
+            //public int BranchID { get; set; }
+            ///// <summary>  
+            ///// 时间  
+            ///// </summary>  
+            //public DateTime NowTime { get; set; }
+            ///// <summary>  
+            ///// 0：正常 1：终端机正常  2： 终端机异常  
+            ///// </summary>  
+            //public int ConnStatus { get; set; }
+            public string accountID{get;set;}
+            public string amount{get;set;}
+            public string Method{get;set;} 
+            public string expireDay{get;set;}
+            public string description{get;set;}
+
+        }
+
+        private void btn_Request_Click(object sender, EventArgs e)
+        {
+            //while (true)
+            //{
+                TcpClient client = new TcpClient((IPAddress.Parse("121.40.242.227")).ToString(), 7002);
+                ConnectionDataMessage cdm = new ConnectionDataMessage();
+                cdm.Method = "FinancialRewardsExecute";
+                cdm.accountID = "753082221526781952";
+                cdm.amount = "1";
+                cdm.expireDay = "test_1";
+                cdm.description = "奖励";
+
+                NetworkStream ns = client.GetStream();
+                if (ns.CanWrite) {
+                    //JavaScriptSerializer serial = new JavaScriptSerializer();
+                    //string context = serial.Serialize(cdm);
+                    //Byte[] sendBytes = Encoding.UTF8.GetBytes(context);
+                    //ns.Write(sendBytes, 0, sendBytes.Length);
+                    //ns.
+                }
+
+
+                //System.Runtime.Serialization.Json.DataContractJsonSerializer dss = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(ConnectionDataMessage));
+                //MemoryStream ms = new MemoryStream();
+                //ConnectionDataMessage dataMessage = new ConnectionDataMessage()
+                //{
+                //    //Address = GetIpAddress(client.Client.LocalEndPoint.ToString()),  
+                //    //SignInName = "az",  
+                //    //BranchID = 12,  
+                //    //DeviceCode = "A31",  
+                //    //NowTime = System.DateTime.Now,  
+                //    //ConnStatus = 0  
+                //    Method = "FinancialRewardsExecute",
+                //    accountID = "753082221526781952",
+                //    amount = "1",
+                //    expireDay = "test_1",
+                //    description = "奖励"
+                //};
+                //dss.WriteObject(ms, dataMessage);
+                //byte[] data = GetConMessage(ms);
+                //NetworkStream stream = client.GetStream();
+
+                //stream.Write(data, 0, data.Length);
+                //ms.Close();
+                //stream.Close();
+
+                client.Close();
+                //Thread.Sleep(1000); 
+            //}
+        }
+    }
+}
